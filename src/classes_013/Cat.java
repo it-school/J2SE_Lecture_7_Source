5982d32f9ecf23df44d86ffad834b3add84cccb5
Viktor Speranskyy <vencedor@ukr.net>
2022-04-12 09:50:39 +0300

    Syntax enhancements 2022.04

--- src/classes_013/Cat.java
+++ src/classes_013/Cat.java
@@ -1,16 +1,62 @@
 package classes_013;
 
 class Cat {
-    // Поле - имя
-    String name;
-    // Поле - цвет
-    String color;
+   // Поле - имя
+   private String name;
+   // Поле - цвет
+   private String color;
+   private float temperature;
 
-    // Метод возвращающий строку с именем кота
-    // и фразой " мяучит..."
-    String Speak() {
-        return name + " мяучит...";
-    }
+   public Cat() {
+      this.setName("");
+      this.setColor("black");
+      this.setTemperature(37f);
+   }
+
+   public Cat(String name, String color, float temperature) {
+      this.setName(name);
+      this.setColor(color);
+      this.setTemperature(temperature);
+   }
+
+   public String getName() {
+      return this.name;
+   }
+
+   public void setName(String name) {
+      this.name = name.strip();
+   }
+
+   public String getColor() {
+      return this.color.toUpperCase();
+   }
+
+   public void setColor(String color) {
+      this.color = color.strip();
+   }
+
+   public float getTemperature() {
+      return ((int) temperature * 10) / 10;
+   }
+
+   public void setTemperature(float temperature) {
+      this.temperature = temperature > 32 && temperature < 43 ? temperature : 37;
+   }
+
+   // Метод возвращающий строку с именем кота
+   // и фразой " мяучит..."
+   String Speak() {
+      return name + " мяучит...";
+   }
+
+   @Override
+   public String toString() {
+      return "Cat{" +
+              name +
+              ", color: '" + color + '\'' +
+              ", temperature: " + temperature +
+              '}';
+   }
 }
 
 
--- src/classes_013/Main.java
+++ src/classes_013/Main.java
@@ -1,33 +1,36 @@
 package classes_013;
 
 public class Main {
-    public static void main(String[] args) {
-        // Создаём первый экземпляр класса Cat
-        Cat cat1 = new Cat();
-        // Создаём второй экземпляр класса Cat
-        Cat cat2 = new Cat();
+   public static void main(String[] args) {
+      // Создаём первый экземпляр класса Cat
+      Cat cat1 = new Cat();
+      // Создаём второй экземпляр класса Cat
+      Cat cat2 = new Cat();
 
-        // Присваиваем полям name и color
-        // Экземпляра класса Cat - cat1
-        // Значения Соня, и Gray
-        cat1.name = "Соня";
-        cat1.color = "Grey";
+      // Присваиваем полям name, color, temperature
+      // Экземпляра класса Cat - cat1
+      // Значения Соня, Gray, 70
+      //cat1.name = "Соня";
+      cat1.setName("Соня");
+      cat1.setColor("Grey");
+      cat1.setTemperature(70);
 
-        // Присваиваем полям name и color
-        // Экземпляра класса Cat - cat2
-        // Значения Мурка, и White
-        cat2.name = "Мурка";
-        cat2.color = "White";
+      // Присваиваем полям name, color, temperature
+      // Экземпляра класса Cat - cat2
+      // Значения Мурка, White, 38
+      cat2.setName("Мурка");
+      cat2.setColor("White");
+      cat2.setTemperature(38);
 
-        System.out.println("Имя первой кошки : " + cat1.name +
-                ", Цвет первой кошки : " + cat1.color);
-        System.out.println("Имя второй кошки : " + cat2.name +
-                ", Цвет второй кошки : " + cat2.color);
+      System.out.println("Имя первой кошки : " + cat1.getName() + ", Цвет первой кошки : " + cat1.getColor() + ", temp = " + cat1.getTemperature());
+      System.out.println(cat1.Speak());
 
-        System.out.println(cat1.Speak());
-    }
+      System.out.println("Имя второй кошки : " + cat2.getName() + ", Цвет второй кошки : " + cat2.getColor() + ", temp = " + cat2.getTemperature());
+      System.out.println(cat2.Speak());
+
+      // Создаём объект с помощью конструктора с параметрами
+      Cat cat3 = new Cat("Grey", "gray", 38);
+      // Выводим полную информацию о нём используя строковое представление объекта toString()
+      System.out.println(cat3);
+   }
 }
-
-
-
-
